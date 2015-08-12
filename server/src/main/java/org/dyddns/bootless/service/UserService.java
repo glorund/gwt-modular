4ceb6470600c6f080b5fa9cc628f66c68932e7e5
glorund <Roman.Cherednychenko@miratechgroup.com>
2015-08-12 18:28:16 +0300

    first scratch

---
+++ server/src/main/java/org/dyddns/bootless/service/UserService.java
@@ -0,0 +1,13 @@
+package org.dyddns.bootless.service;
+
+import org.dyddns.bootless.User;
+
+/**
+ * Created by glorund on 8/12/2015.
+ */
+public class UserService {
+    public User getUser() {
+        return new User();
+    }
+
+}
